76674eb58d175cf8f64cadffd8623ce3aa9a2b1f
abhishekkura1 <abhishek.kura1@gmail.com>
2025-08-16 00:38:55 +0530

    Demo for CryptoJS

--- src/tests/loginTest.spec.ts
+++ src/tests/loginTest.spec.ts
@@ -1,12 +1,30 @@
 import {test} from "@playwright/test";
 import LoginPage from "../pages/LoginPage";
+import {decrypt,encrypt} from "../utils/CryptojsUtil";
+import {encryptEnvFile,decryptEnvFile} from "../utils/EncryptEnvFile";
 
-test("test",async({page})=>
+test("test",async({page})=> //test.skip("test",async({page})=>
 {
     const loginPage=new LoginPage(page);
     await loginPage.navigateToLoginPage();
-    await loginPage.fillUsername("abhishek5225278@agentforce.com");
-    await loginPage.fillPassword("test1234");
+    /*await loginPage.fillUsername("abhishek5225278@agentforce.com");
+    await loginPage.fillPassword("test1234");*/
+    await loginPage.fillUsername(process.env.userid!);
+    await loginPage.fillPassword(process.env.password!);
     const homePage=await loginPage.clickLoginButton();
     await homePage.expectSalesTitleToBeVisible();
+});
+
+test("Sample env test",async({page})=>
+{
+    /*console.log(process.env.NODE_ENV);
+    console.log(process.env.userid);
+    console.log(process.env.password);*/
+    /*const plaintext='Hello, Mars!';
+    const encryptedText=encrypt(plaintext);
+    console.log('SALT:',process.env.SALT);
+    console.log('Encrypted:',encryptedText);
+    const decryptedText=decrypt(encryptedText);
+    console.log('Decrypted:',decryptedText);*/
+    encryptEnvFile();
 });
